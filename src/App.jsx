7ea2a6bbad742ee2e5fae22594fbbee58d54a0a2
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-03-04 23:49:13 +0530

    Added logic related result block

--- src/App.jsx
+++ src/App.jsx
@@ -5,55 +5,78 @@ import { calculateInvestmentResults, formatter } from "./util/investment";
 
 function App() {
   const [inputs, setInputs] = useState({
-    initial_investment: 0,
-    annual_investment: 0,
-    expected_return: 0,
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
     duration: 0,
   });
 
+  // const [annualResult, setAnnualResult] = useState([]);
+
   function handleValueChange(event) {
-    const {name, value} = event.target
+    const { name, value } = event.target;
+    const numericValue = value === '' ? 0 : Number(value);
     setInputs((prevValue) => {
       return {
         ...prevValue,
-        [name]: value,
+        [name]: numericValue,
       };
     });
   }
 
-  function deriveAnnualData(){
-  //   const allValuesNonZero = Object.values(inputs).every(value => {
-  //     console.log(inputs)
-  //     console.log(value)
-  //     return (value !== 0)
-  // })
-  //   if(allValuesNonZero){
-  //     setAnnualResult(calculateInvestmentResults(inputs));
-  //   }
-  //   console.log(allValuesNonZero)
-  //   console.log(annualResult)
+  function deriveAnnualData(inputs) {
+    const allValuesNonZero = Object.values(inputs).every((value) => {
+      return value !== 0;
+    });
+    if (allValuesNonZero && inputs.duration > 0) {
+      const annualData = calculateInvestmentResults(inputs);
+      let totalInterest = 0
+      console.log(annualData);
+      return annualData.map((data, index) => {
+        totalInterest += data.interest;
+        const investedCapital = inputs.initialInvestment + (inputs.annualInvestment * (index + 1));
+
+        return (
+          <tr key={index}>
+            <td>{data.year}</td>
+            <td>{data.valueEndOfYear}</td>
+            <td>{data.interest}</td>
+            <td>{totalInterest}</td>
+            <td>{investedCapital}</td>
+          </tr>
+        )
+      })
+    }
+
+      return (
+        <tr>
+          <td colSpan="5" style={{ textAlign: 'center' }}>
+            Please enter all values to see the investment results
+          </td>
+        </tr>
+      ); 
   }
 
-  console.log(inputs);
+  // console.log(inputs);
   return (
     <>
       <Header title="Investment Calculator" />
       <div id="user-input" className="input-group">
         <Input
-          initialValue={inputs.initial_investment}
-          name="initial_investment"
+          initialValue={inputs.initialInvestment}
+          name="initialInvestment"
           label="Initial Investment"
           onValueChange={handleValueChange}
         />
         <Input
-          initialValue={inputs.annual_investment}
-          name="annual_investment"
+          initialValue={inputs.annualInvestment}
+          name="annualInvestment"
           label="Annual Investment"
           onValueChange={handleValueChange}
         />
         <Input
-          initialValue={inputs.expected_return}
-          name="expected_return"
+          initialValue={inputs.expectedReturn}
+          name="expectedReturn"
           label="Expected Return"
           onValueChange={handleValueChange}
         />
@@ -67,16 +90,14 @@ function App() {
       <table id="result">
         <thead>
           <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest(Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest(Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
           </tr>
         </thead>
-        <tbody>
-          {/* {deriveAnnualData(inputs)} */}
-        </tbody>
+        <tbody>{deriveAnnualData(inputs)}</tbody>
       </table>
     </>
   );
